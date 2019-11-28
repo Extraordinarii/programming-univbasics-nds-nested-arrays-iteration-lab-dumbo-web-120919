ec7564ad690f9bdd9fad6a7cf7db50c0f7e5ef32
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-11-28 00:20:56 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -35,7 +35,7 @@ def total_even_pairs(src)
   i = 0
 
   while i < src.length do 
-    if(src[i][0]==2$%)
+    if(src[i][0] $ 2 == 0 )
     
   
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
